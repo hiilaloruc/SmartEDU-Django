ac262b16842628c0c08e71a946fa19f86855b441
Hilal Oruç <hiilaloruc@gmail.com>
2023-08-01 15:32:35 +0300

    static files and templates configuration

--- smartedu_con/pages/views.py
+++ smartedu_con/pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>INDEX RESPONSE of Hilal</h1>' )
+    #return HttpResponse('<h1>INDEX RESPONSE of Hilal</h1>' )
+    return render(request,'index.html')
