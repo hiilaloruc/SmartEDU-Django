8c4ebfdfa41493769160a8a9f911764a5e30f787
Hilal Oruç <hiilaloruc@gmail.com>
2023-08-02 16:57:35 +0300

    course page ui completed and dynamically courses conn with db

--- smartedu_con/courses/views.py
+++ smartedu_con/courses/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from . models import Course
 # Create your views here.
+
+def course_list(request):
+    courses = Course.objects.all()
+    #create a context variable to send some datas to the courses.html page.
+    context = {
+        'courses': courses.order_by('-date')
+    }
+    return render(request, 'courses.html',context)
